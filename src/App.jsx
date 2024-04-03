8bcefdcf5b7eb93e532ff743045f703a03a27296
Vihaan Vulpala <vihaanv14@gmail.com>
2024-04-03 10:36:30 -0400

    basic api implementation working

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("")
+  const url = `https://api.agify.io?name=${name}`
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+
+    axios.get(url).then(() => console.log("WORKING")).catch(() => console.log("NOT WORKING"))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Name Search Dashboard</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Name: </label>
+        <input value={name} onChange={e => (setName(e.target.value))}></input>
+        <div>
+          <button>Search</button>
+        </div>
+      </form>
     </>
   )
 }
